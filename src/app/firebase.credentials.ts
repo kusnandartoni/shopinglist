d13bf94f694db2d4af3c5b3a33f352583c808513
kusnandartoni <kusnandartoni@gmail.com>
2018-05-09 13:15:35 +0700

    initial commit

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDbkksd4uqZh5lkf1Xmmh7dWsniTWgllig",
+    authDomain: "shopinglist-ionic.firebaseapp.com",
+    databaseURL: "https://shopinglist-ionic.firebaseio.com",
+    projectId: "shopinglist-ionic",
+    storageBucket: "shopinglist-ionic.appspot.com",
+    messagingSenderId: "85648706981"
+};
